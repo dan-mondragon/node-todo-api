721e71160b874bc8e60d22d0076ca93062398b65
Ricardo <danmleal@gmail.com>
2017-08-09 12:34:34 -0500

    Test the DELETE /todos/:id

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -82,3 +82,39 @@ describe('GET /todos/:id', () => {
         }).end(done);
     });
 });
+
+describe('DELETE /todos/:id', () => {
+    it('Should remove a todo', (done) => {
+        var hexId = todos[1]._id.toHexString();
+        request(app)
+        .delete(`/todos/${hexId}`)
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo._id).toBe(hexId);
+        }).end((err, res) => {
+            if(err){
+                return done(err);
+            }
+
+            Todo.findById(hexId).then((todo) => {
+                expect(todo).toNotExist();
+                done();
+            }).catch((err) => done(err));
+        });
+    });
+
+    it('Should return 404 if todo not found', (done) => {
+        var hexId = new ObjectID().toHexString();
+        request(app)
+        .delete('/todos/${hexId}')
+        .expect(404)
+        .end(done);
+    });
+
+    it('Should return 404 if objectid is invalid', (done) => {
+        request(app)
+        .delete('/todos/123abc')
+        .expect(404)
+        .end(done);
+    });
+});
