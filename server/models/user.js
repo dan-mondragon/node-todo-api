d136b03055a2ec96cd1840b84980e69e943e7040
Ricardo <danmleal@gmail.com>
2017-10-16 17:32:26 -0500

    Beginning class 88

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,31 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
 
 var User = mongoose.model('User', {    
-email: {
+    email: {
+            type: String,
+            required: true,
+            trim: true,
+            minlength: 1,
+            unique: true,
+            validate: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        },
+    password: {
         type: String,
-        required: true,
-        trim: true,
-        minlength: 1
-    }
+        require: true,
+        minlength: 6 
+    },
+    tokens: [{
+        access: { 
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
 
 module.exports = {User};
