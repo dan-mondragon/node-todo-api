2a6ced1d1f573cf485b87bbd667ea40f0dc4ffa7
Ricardo <danmleal@gmail.com>
2017-10-20 11:23:37 -0500

    Add tests for GET /users/me and POST /users

---
+++ server/test/seed/seed.js
@@ -0,0 +1,51 @@
+const {ObjectID} = require('mongodb');
+const {Todo} = require('./../../models/todo');
+const {User} = require('./../../models/user');
+const jwt = require('jsonwebtoken');
+
+const userOneId = ObjectID();
+const userTwoId = ObjectID();
+const users = [{
+    _id: userOneId,
+    email: 'daniel.mon@gigigo.com.mx',
+    password: 'userOnePass',
+    tokens: [{
+        access: 'auth',
+        token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString()
+    }]
+},{
+    _id: userTwoId,
+    email: 'daniel@gigigo.com.mx',
+    password: 'userTwoPass'
+}];
+
+const todos = [{
+    _id: new ObjectID(),
+    text: 'First test todo'
+}, {
+    _id: new ObjectID(),
+    text: 'Second test todo',
+    completed: true,
+    completedAt: 333
+}];
+
+const populateTodos = (done) => {
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
+};
+
+const populateUsers = (done) => {
+    User.remove({}).then(() => {
+        var userOne = new User(users[0]).save();
+        var userTwo = new User(users[1]).save();
+        return Promise.all([userOne, userTwo]);
+    }).then(() => done());
+};
+
+module.exports = {
+    todos,
+    populateTodos,
+    users,
+    populateUsers
+}
--- server/test/server.test.js
+++ server/test/server.test.js
@@ -3,22 +3,11 @@ const request = require('supertest');
 const {ObjectID} = require('mongodb');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
+const {User} = require('./../models/user');
 
-const todos = [{
-    _id: new ObjectID(),
-    text: 'First test todo'
-}, {
-    _id: new ObjectID(),
-    text: 'Second test todo',
-    completed: true,
-    completedAt: 333
-}];
-
-beforeEach((done) => {
-    Todo.remove({}).then(() => {
-        return Todo.insertMany(todos);
-    }).then(() => done());
-});
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe('POST /todos', () => {
     it('should create a new todo', (done) => {
@@ -150,5 +139,75 @@ describe('PATCH /todos/:id', () => {
             expect(res.body.todo.completed).toBe(completed);
             expect(res.body.todo.completedAt).toNotExist();
         }).end(done);
+    });
+});
+
+describe('GET /users/me', () => {
+    it('should return user if authenticated', (done) => {
+        request(app)
+        .get('/users/me')
+        .set('x-auth', users[0].tokens[0].token)
+        .expect(200)
+        .expect((res) => {
+            expect(res.body._id).toBe(users[0]._id.toHexString());
+            expect(res.body.email).toBe(users[0].email);
+        })
+        .end(done);
+    });
+
+    it('should return 401 if not authenticated', (done) => {
+        request(app)
+        .get('/users/me')
+        .expect(401)
+        .expect((res) => {
+            expect(res.body == {});
+        })
+        .end(done);
+    });
+});
+
+describe('POST /users', () => {
+    it('should create a user', (done) => {
+        var email = 'example@example.com';
+        var password = '123abc!';
+        request(app)
+        .post('/users')
+        .send({email, password})
+        .expect(200)
+        .expect((res) => {
+            expect(res.headers['x-auth']).toExist();
+            expect(res.body.email).toExist();
+        })
+        .end((err) => {
+            if(err){
+                return done(err);
+            }
+
+            User.findOne({email}).then((user) => {
+                expect(user).toExist();
+                expect(user.password).toNotBe(password);
+                done();
+            });
+        });
+    });
+
+    it('should return validation errors if request invalid', (done) => {
+        var email = 'example.com';
+        var password = '123abc!';
+        request(app)
+        .post('/users')
+        .send({email, password})
+        .expect(400)
+        .end(done);
+    });
+
+    it('should not create user if email in use', (done) => {
+        var email = 'daniel@gigigo.com.mx';
+        var password = '123abc!';
+        request(app)
+        .post('/users')
+        .send({email, password})
+        .expect(400)
+        .end(done);
     });
 });
