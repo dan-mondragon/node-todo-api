755c0d3c11e41bcd3858a106edda5a43abbb4319
Ricardo <danmleal@gmail.com>
2017-10-20 12:06:51 -0500

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -67,6 +67,24 @@ UserSchema.statics.findByToken = function(token) {
     })
 };
 
+UserSchema.statics.findByCredentials = function(email, password){
+    var User = this;
+    return User.findOne({email}).then((user) => {
+        if(!user){
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if(res){
+                    return resolve(user);
+                }
+                return reject(err);
+            });
+        });
+    });
+}
+
 UserSchema.pre('save', function(next) {
     var user = this;
     if(user.isModified('password')){
