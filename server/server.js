6730cc71f008130be7f6832c06f25f7d11f1fd4a
Ricardo <danmleal@gmail.com>
2017-10-20 12:49:51 -0500

    Tests for POST /users/login

--- server/server.js
+++ server/server.js
@@ -115,7 +115,7 @@ app.get('/users/me', authenticate, (req,res) => {
 app.post('/users/login', (req,res) => {
     var body = _.pick(req.body, ['email', 'password']);
     User.findByCredentials(body.email, body.password).then((user) => {
-        return user.generateAuthToken().then((token) =>{
+        user.generateAuthToken().then((token) =>{
             res.header('x-auth', token).send(user);
         });
     })
